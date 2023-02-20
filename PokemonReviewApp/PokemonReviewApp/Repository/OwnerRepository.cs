9506a0fe1946f7b9f1a58991f80ee460125668be
Azeem Akram <aakram@uh.edu>
2023-02-20 16:42:42 -0600

    Complete OwnerController
    Complete OwnerController

---
+++ PokemonReviewApp/PokemonReviewApp/Repository/OwnerRepository.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PokemonReviewApp.Data;
+using PokemonReviewApp.Interfaces;
+using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Repository
+{
+    public class OwnerRepository : IOwnerRepository
+    {
+        private readonly DataContext _context;
+
+        public OwnerRepository(DataContext context)
+        {
+            _context = context;
+        }
+        public Owner GetOwner(int ownerId)
+        {
+            return _context.Owners.Where(o => o.Id == ownerId).Include(p => p.Country).FirstOrDefault();
+        }
+
+        public ICollection<Owner> GetOwnersOfAPokemon(int pokeId)
+        {
+            return _context.PokemonOwners.Where(p => p.PokemonId == pokeId).Select(o => o.Owner).Include(p => p.Country).ToList();
+        }
+
+        public ICollection<Owner> GetOwners()
+        {
+            return _context.Owners.OrderBy(o => o.Id).Include(p => p.Country).ToList();
+        }
+
+        public ICollection<Pokemon> GetPokemonByOwner(int ownerId)
+        {
+            return _context.PokemonOwners.Where(po => po.OwnerId == ownerId).Select(p => p.Pokemon).ToList();
+        }
+
+        public bool OwnerExists(int ownerId)
+        {
+            return _context.Owners.Any(o => o.Id == ownerId);
+        }
+    }
+}
